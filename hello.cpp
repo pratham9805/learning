ec86db41b752a0e7f85d98051bff95165fc52409
Pratham Patel <patelpratham580@gmail.com>
2026-01-01 16:47:57 +0530

    first commit

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+   
+    for(int row=1;row<=6;row++){
+            
+        for(int col=row;col>=1;col--){
+            cout<<col<<" ";
+        }
+        cout<<endl;
+        
+    }
+
+    return 0;
+   
+
+}
